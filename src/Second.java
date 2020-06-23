772c4dd8d35d965c54d6896aabef485c1c119c96
silenc3502 <gcccompil3r@gmail.com>
2020-06-23 13:59:54 +0900

    Second Init

--- src/Second.java
+++ src/Second.java
@@ -5,10 +5,15 @@ public class Second {
         Scanner scan = new Scanner(System.in);
         System.out.println("Second Test");
 
-        System.out.print("값을 입력해주세요: ");
-        int num = scan.nextInt();
+        System.out.print("첫 번째 정수를 입력해주세요: ");
+        int num1 = scan.nextInt();
 
-        System.out.println("res = " + num);
+        System.out.print("두 번째 정수를 입력해주세요: ");
+        int num2 = scan.nextInt();
+
+        if(num1 < num2) {
+            System.out.printf("%d는(은) %d 보다 작다.\n", num1, num2);
+        }
     }
 }
 
