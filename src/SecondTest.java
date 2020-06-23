e22864ddf4732a94e83c91921600feb9f8ee5aff
silenc3502 <gcccompil3r@gmail.com>
2020-06-23 16:04:09 +0900

    Add While Test

--- src/SecondTest.java
+++ src/SecondTest.java
@@ -1,5 +1,16 @@
 public class SecondTest {
     public static void main(String[] args) {
         System.out.println("문제 은행");
+
+        // 1. 1 ~ 20까지 숫자중 짝수를 출력해보세요.
+        // 1-1. 1 ~ 20까지 출력을 해보자!
+        // 1-2. 짝수를 어떻게 찾을까 ?
+        //      Scanner, if를 사용해서
+        //      입력값이 짝수인지 홀수인지 판별을 어떻게 할것인가
+        // 1-3. 1 ~ 20까지 출력한 부분에 해당 내용을 결합하자!
+        // while + if, for + if
+
+        // 2. 1 ~ 20까지 숫자중 홀수의 합을 구하세요.
+        // while + if, for + if
     }
 }
