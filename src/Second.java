fb974c653b915a177392ec8b81be23ea2ac9bab4
silenc3502 <gcccompil3r@gmail.com>
2020-06-23 13:19:06 +0900

    Second Init

--- src/Second.java
+++ src/Second.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Second {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Second Test");
     }
 }
