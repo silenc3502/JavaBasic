c589db1a0b92f93078acae605f0c7ef89ba062d4
silenc3502 <gcccompil3r@gmail.com>
2020-06-23 13:41:56 +0900

    pull test

--- src/Second.java
+++ src/Second.java
@@ -4,5 +4,12 @@ public class Second {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Second Test");
+
+        System.out.print("값을 입력해주세요: ");
+        int num = scan.nextInt();
+
+        System.out.println("res = " + num);
     }
 }
+
+
