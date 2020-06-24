7376736ebee79e9c9f79aa08e1a2d75e8f031d4d
silenc3502 <gcccompil3r@gmail.com>
2020-06-24 15:42:51 +0900

    Array

--- src/Third/RandArrayClassTest.java
+++ src/Third/RandArrayClassTest.java
@@ -9,13 +9,19 @@ class Location {
     int[] locXArr;
     int[] locYArr;
 
+    float[] distArr;
+
     public Location() {
         locXArr = new int[MAX];
         locYArr = new int[MAX];
 
         for(int i = 0; i < MAX; i++) {
-
+            // locXArr와 locYArr를 채운다.
         }
+    }
+
+    public void calcDistance() {
+        // distArr를 구한다.
     }
 }
 // 문제 1. 현재 내가 있는 위치가 (3, 5)에 있다.
@@ -23,6 +29,12 @@ class Location {
 //        7개의 랜덤 좌표를 생성하자!
 //        (생성 범위는 2 ~ 7로 만들자)
 //        각 좌표의 거리값을 표현하시오.
+
+// 현재 만들어진 코드를 기반으로
+// 아래 작업을 진행하면 문제를 풀 수 있다.
+// 1. 생성자 작성을 완료한다.
+//    반복을 하면선 랜덤 좌표를 생성하는 작업
+// 2. calcDistance() 매서드를 작성한다.
 public class RandArrayClassTest {
     public static void main(String[] args) {
         System.out.println("랜덤 좌표 문제");
